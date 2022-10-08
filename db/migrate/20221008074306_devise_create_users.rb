7d55100b1d069ba393a977d42b052242e639a9d8
Hiro0608 <h.satou1991@gmail.com>
2022-10-08 18:07:32 +0900

    SNSログイン機能

--- db/migrate/20221008074306_devise_create_users.rb
+++ db/migrate/20221008074306_devise_create_users.rb
@@ -4,8 +4,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       t.string :nickname, null: false
-      t.string :lastname, null: false
       t.string :firstname, null: false
+      t.string :lastname, null: false
       t.date   :birthday, null: false
       ## Database authenticatable
       t.string :email,              null: false, default: ""
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_08_074306) do
+ActiveRecord::Schema.define(version: 2022_10_08_081709) do
+
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
-    t.string "lastname", null: false
     t.string "firstname", null: false
+    t.string "lastname", null: false
     t.date "birthday", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -28,4 +37,5 @@ ActiveRecord::Schema.define(version: 2022_10_08_074306) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "sns_credentials", "users"
 end
